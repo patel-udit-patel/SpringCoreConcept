0b7721066c13b1480d956bd9cfd656927e22e547
patel-udit-patel <udit0428t@gmail.com>
2025-02-23 20:44:35 +0530

    [Udit Patel] Add. Spring concepts Using VS code

---
+++ spring-core-concepts/src/test/java/com/example/spring_core_concepts/SpringCoreConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_core_concepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
