3ebadd51f47061b4d9cf91a5036ea8562fea95a7
patel-udit-patel <udit0428t@gmail.com>
2025-02-24 21:30:20 +0530

    [Udit Patel] Add.Add Logger to Log Messages

--- spring-core-concepts/src/main/java/com/example/spring_core_concepts/controller/HelloController.java
+++ spring-core-concepts/src/main/java/com/example/spring_core_concepts/controller/HelloController.java
@@ -3,13 +3,25 @@ package com.example.spring_core_concepts.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/hello")
     public String sayHello() {
+        return "Hello, Spring Boot!";
+    }
+
+
+    @GetMapping("/helloworld")
+    public String sayHelloWithLogger() {
+        logger.info("Received request for /hello endpoint");
+        logger.debug("Processing request in sayHello method");
         return "Hello, Spring Boot!";
     }
 }
